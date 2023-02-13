7f7edd39cf251cb8c0c4f85d399b689fe1bbf0b4
Nishantr03 <nishantrastogi2003@gmail.com>
2023-02-13 17:28:03 +0530

    first commit

---
+++ src/main/java/devops/b1/h/Myclass.java
@@ -0,0 +1,15 @@
+package devops.b1.h;
+
+public class Myclass {
+	public void show() {
+		System.out.println("Hello Devops Welcome to Jenkins");
+	}
+	
+
+	public static void main(String[] args) {
+		Myclass obj = new Myclass();
+		obj.show();
+
+	}
+
+}
